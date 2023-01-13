1ef8d5a1bc09dab48bb30fb198ebc1c6c988ed3c
EverEmpty <114514553+EverEmpty@users.noreply.github.com>
2023-01-13 16:41:48 +0300

    Add files via upload

---
+++ CalcException.java
@@ -0,0 +1,12 @@
+package calculatorm;
+
+public class CalcException extends Exception {
+    public CalcException() {
+        System.out.println("Ошибка");
+    }
+    
+    public CalcException(String message){
+        this();
+        System.out.println(message);
+    }
+}

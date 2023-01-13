607b72fb0b2f2c3b243232863ef749b11beec4c5
EverEmpty <114514553+EverEmpty@users.noreply.github.com>
2023-01-13 17:03:04 +0300

    Add files via upload

---
+++ CalcException.java
@@ -0,0 +1,12 @@
+package calcRimArab;
+
+public class CalcException extends Exception {
+    public CalcException() {
+        System.out.println("Ошибка");
+    }
+    
+    public CalcException(String message){
+        this();
+        System.out.println(message);
+    }
+}
---
+++ CalculatorMain.java
@@ -0,0 +1,25 @@
+package calcRimArab;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CalculatorMain {
+
+    public static void main(String[] args) throws Exception {
+        try{
+            System.out.println("Калькулятор для выражений вида: \"Число1 Операция Число2\", через пробел. Допускаются числа от 1 до 10 или от I до X включительно. Операции: + - * /");
+            System.out.print("Введите выражение: ");
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+            String calcString = bReader.readLine();
+            
+            Calc calc = new Calc();
+            String result = calc.result(calcString);
+            System.out.println("Ответ: " + result);
+        } 
+        catch(CalcException | IOException e){
+            
+        }
+
+    }
+    
+}
